b0a5d9cf3f8ac26decbb2c9dc03b56e3c3762a86
tyx2000 <tyx2000@gmail.com>
2025-05-12 14:53:32 +0800

    feat: pagespeed review

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,8 @@ const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-  style-src 'self' 'nonce-${nonce}';
+  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-inline' https: http:;
+  style-src 'self' 'unsafe-inline';
   img-src 'self' blob: data:;
   font-src 'self';
   object-src 'none';
